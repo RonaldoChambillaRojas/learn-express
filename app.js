fcc137395ce0728efd0635cdedfcc48854d7a465
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2026-01-05 23:08:55 -0500

    refactor(app): handle async errors via centralized middleware

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/api/users', usersRoutes);
  * Middleware de errores
  */
 app.use((err, req, res, next) => {
-    console.error(err.stack);
+    console.error(err);
     res.status(500).json({
         error: 'Error interno del servidor'
     });
