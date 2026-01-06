343998ec4738b73c6238b9f6358a48a972d15fe7
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2026-01-05 23:05:57 -0500

    chore(fs): use fs/promises for async file operations

--- services/users.service.js
+++ services/users.service.js
@@ -1,67 +1,18 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 
 const userFilePath = path.join(__dirname, '..', 'users.json');
 
-const readUsers = () => {
-    const data = fs.readFileSync(userFilePath, 'utf-8');
+const readUsers = async () => {
+    const data = await fs.readFile(userFilePath, 'utf-8');
     return JSON.parse(data);
 };
 
-const writeUsers = (users) => {
-    fs.writeFileSync(userFilePath, JSON.stringify(users, null, 2));
-};
-
-const getAllUsers = () => readUsers();
-
-const getUserById = (id) => {
-    const users = readUsers();
-    return users.find(u => u.id === Number(id));
-};
-
-const createUser = (name, email) => {
-    const users = readUsers();
-
-    const newUser = {
-        id: Date.now(),
-        name,
-        email
-    };
-
-    users.push(newUser);
-    writeUsers(users);
-    return newUser;
-};
-
-const updateUser = (id, data) => {
-    const users = readUsers();
-    const index = users.findIndex(u => u.id === Number(id));
-
-    if (index === -1) return null;
-
-    users[index] = {
-        ...users[index],
-        ...data
-    };
-
-    writeUsers(users);
-    return users[index];
-};
-
-const deleteUser = (id) => {
-    const users = readUsers();
-    const filtered = users.filter(u => u.id !== Number(id));
-
-    if (filtered.length === users.length) return false;
-
-    writeUsers(filtered);
-    return true;
+const writeUsers = async (users) => {
+    await fs.writeFile(userFilePath, JSON.stringify(users, null, 2));
 };
 
 module.exports = {
-    getAllUsers,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUser
+    readUsers,
+    writeUsers
 };
